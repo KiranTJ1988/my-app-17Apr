187f0d925b1337c1d1c02d86f038b8b204cab0e6
KiranTJ1988 <45386831+KiranTJ1988@users.noreply.github.com>
2023-04-19 09:19:12 +0530

    Added jenkins comment

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! modified by Kiran" );
+        System.out.println( "Hello World! modified by Kiran and added this from Jenkins" );
     }
 }
